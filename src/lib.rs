05a828c8f064cb448b5b9ec54cedfbc5b0cdf769
RustyStriker <avivr903@tutanota.com>
2021-09-27 13:33:42 +0300

    system public module

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,13 @@ pub mod plugin;
 pub mod transform_mode;
 pub mod shapes;
 
+pub mod systems {
+    //! Re-exports all the systems in the crate for ease of access
+    pub use super::broad::broad_phase_1;
+    pub use super::narrow::narrow_phase_system;
+    pub use super::normal_coll::{broad_phase_2, narrow_phase_2, ray_phase, CollPairKin, CollPairSensor, CollPairStatic};
+}
+
 pub mod prelude {
     //! This module re-exports all the things you might need for 2d physics
     //! simulation.
